c1b9ee3e75372ee26989a144fc0cad42d07d6df5
Matvey <realsmych@gmail.com>
2023-04-23 22:21:32 +0100

    Initial commit with Homebrew and Vim

---
+++ Homebrew/brew_install.sh
@@ -0,0 +1,9 @@
+brew install gh
+brew install gradle
+brew install openjdk
+
+brew install --cask jetbrains-toolbox
+brew install --cask outline-manager
+brew install --cask safeincloud-password-manager
+brew install --cask telegram
+brew install --cask warp
---
+++ README.md
@@ -0,0 +1,13 @@
+# Apéritif
+
+### Homebrew
+```
+cd Homebrew
+chmod +x brew_install.sh
+./brew_install.sh
+```
+
+### Vim
+```
+cp Vim/vim_config ~/.vimrc
+```

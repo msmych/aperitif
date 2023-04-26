98df46521dde4f842533e3c9ce9cb1c5beb92bb8
Matvey <realsmych@gmail.com>
2023-04-27 00:04:48 +0100

    Vim Netrw config

--- Homebrew/brew_install.sh
+++ Homebrew/brew_install.sh
@@ -2,6 +2,7 @@ brew install gh
 brew install gradle
 brew install openjdk
 
+brew install --cask discord
 brew install --cask jetbrains-toolbox
 brew install --cask outline-manager
 brew install --cask safeincloud-password-manager

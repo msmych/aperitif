e0e988bc6606c5f2758caa3c9fd623eb3fc3ea6e
Matvey <realsmych@gmail.com>
2023-10-01 23:40:05 +0100

    Telescope keymaps

--- Nvim/after/plugin/telescope.lua
+++ Nvim/after/plugin/telescope.lua
@@ -1,6 +1,18 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>?', builtin.oldfiles, {})
+vim.keymap.set('n', '<leader><space>', builtin.buffers, {})
+vim.keymap.set('n', '<leader>/', function()
+  builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, {})
+vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>sw', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>sd', builtin.diagnostics, {})
+vim.keymap.set('n', '<leader>sr', builtin.resume, {})
+vim.keymap.set('n', '<leader>sk', builtin.keymaps, {})

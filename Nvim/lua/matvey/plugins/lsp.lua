3ef300f6a4eda3242e4d084739b1bab9a0311849
Matvey <realsmych@gmail.com>
2024-02-17 20:40:00 +0000

    Nvim cmp tabs

--- Nvim/lua/matvey/plugins/lsp.lua
+++ Nvim/lua/matvey/plugins/lsp.lua
@@ -98,47 +98,4 @@ return {
       }
     end
   },
-
-  'hrsh7th/cmp-nvim-lsp',
-  {
-    'hrsh7th/nvim-cmp',
-    priority = 30,
-    dependencies = {
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-      'hrsh7th/cmp-nvim-lsp',
-      'rafamadriz/friendly-snippets',
-    },
-    config = function()
-      local cmp = require('cmp')
-
-      require("luasnip.loaders.from_vscode").lazy_load()
-
-      local luasnip = require('luasnip')
-      luasnip.config.setup {}
-
-      cmp.setup {
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert {
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          },
-
-        },
-        sources = {
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'buffer' },
-        }
-      }
-    end
-  },
-
 }
